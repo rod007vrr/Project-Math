a7157ad2c92368f0000f25335fe5d184216803fb
rod007vrr <37837157+rod007vrr@users.noreply.github.com>
2018-03-28 13:24:42 -0400

    Deleted duplicate

--- Project Math/Individual Math Programs/Primes test.py
+++
@@ -1,9 +0,0 @@
-howMuch = int(input("Up to what number would you like to check for primes? "))
-for n in range (2,howMuch+1):
-    for x in range (2,n):
-        if n % x == 0:
-            print(n, "is not prime")
-            break
-    else:
-        print(n, "is a prime number")
-            
