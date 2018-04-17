7dcc9fa8cd9cf6b07843052f20c8e03128959d1b
rod007vrr <37837157+rod007vrr@users.noreply.github.com>
2018-04-17 13:43:37 -0400

    Added (6)
    Small issue with y intercept later resolved by finding rule mathematically

---
+++ Project Math/Individual Math Programs/(6)Linear Equation Finder.py
@@ -0,0 +1,8 @@
+print("Hello, welcome to the linear function finder. Input the coordinates of two points to recieve a linear function which passes through both of them")
+x1 = float(input("X value for coordinate 1\n"))
+y1 = float(input("Y value for coordinate 1\n"))
+x2 = float(input("X value for coordinate 2\n"))
+y2 = float(input("Y value for coordinate 2\n"))
+slope = ((y2-y1)/(x2-x1))
+intercept = (y2-(slope * x2))
+print("Equation is",slope ,("x"),"+",intercept)
