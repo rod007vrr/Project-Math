68c499bb09d0bffefc542be6ac71827569cc18f7
rod007vrr <37837157+rod007vrr@users.noreply.github.com>
2018-08-23 22:10:37 -0400

    Added trig rules
    Currently only has rule 1 done start of rules 2 and 5

---
+++ Project Math/Individual Math Programs/(8)Trigonometry rules.py
@@ -0,0 +1,3 @@
+import math
+print("Hello, welcome to the Trignometry rule system! Follow directions to recieve your answer.")
+print("Type 1 to choose Sine rule to find missing side.\n Type 2 to choose sine rule to find missing angle.\nType 3 to choose cosine rule for missing side.\nType 4 to choose cosine rule for missing angle.\nType 5 to find the height of an object from the distance between two angles.\n Type 6 to find the area of a triangle from 2 sides and an angle inbetween")
