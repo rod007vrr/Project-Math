2fd1445aa9771aec6b33c3265a5f3b72f86b5aa3
rod007vrr <37837157+rod007vrr@users.noreply.github.com>
2018-08-23 22:08:25 -0400

    Added conditionals
    If slope = 0 don't print slope
    If Y-Intercept = 0 don't print Y-Intercept
    Also conditionals to print y=mx-b for negatives instead of y=mx+-b

--- Project Math/Individual Math Programs/(6)Linear Equation Finder.py
+++ Project Math/Individual Math Programs/(6)Linear Equation Finder.py
@@ -1,8 +1,16 @@
-print("Hello, welcome to the linear function finder. Input the coordinates of two points to recieve a linear function which passes through both of them")
-x1 = float(input("X value for coordinate 1\n"))
-y1 = float(input("Y value for coordinate 1\n"))
-x2 = float(input("X value for coordinate 2\n"))
-y2 = float(input("Y value for coordinate 2\n"))
-slope = ((y2-y1)/(x2-x1))
-intercept = (y2-(slope * x2))
-print("Equation is",slope ,"x +",intercept)
+print("Hello, welcome to the equation of a linear function. Input two points on the line and I will give you the slope and y-intercept")
+x1 = int(input("X coordinate of point 1\n"))
+y1 = int(input("Y coordinate of point 1\n"))
+x2 = int(input("X coordinate of point 2\n"))
+y2 = int(input("Y coordinate of point 2\n"))
+slope = (y2-y1)/(x2-x1)
+yIntercept = y1-(x1*slope)
+
+if slope == 0:
+    print("y=",yIntercept)
+elif yIntercept == 0:
+    print("y=",slope,"x")
+elif slope != 0 and yIntercept !=0 and slope > 0:
+    print("The equation of your line is y = ",slope,"x +",yIntercept)
+elif slope != 0 and yIntercept !=0 and slope < 0:
+    print("The equation of your line is y = ",slope,"x ",yIntercept)
