6a07319979b8956a0578bfbae361e08c79533a29
rod007vrr <37837157+rod007vrr@users.noreply.github.com>
2018-09-12 18:17:25 -0400

    Updated to final code since I forgot to save

--- MYP 1-3 programs/6th Grade/(1)Numbers/(1)Place Value & Rounding/(1)Convert from different bases.py
+++ MYP 1-3 programs/6th Grade/(1)Numbers/(1)Place Value & Rounding/(1)Convert from different bases.py
@@ -1,6 +1,7 @@
 print("Hello, welcome to the different base converter. To use this just input what number you would like to convert to base 10 and the base to convert from")
-baseToConvertFrom = int(input("Base to convert from:\n"))
-numberToConvert = int(input("Number to convert to base 10\n"))
-print(
-    (int(str(numberToConvert)[len(str(numberToConvert))-1]) * baseToConvertFrom**0) + (int(str(numberToConvert)[len(str(numberToConvert))-2]) * baseToConvertFrom**1)
-)
+inputBase = int(input("Base to convert from\n"))
+numberToConvert = int(input("Number to convert\n"))
+finalVal = 0
+for n in range(0,len(str(numberToConvert))):
+    finalVal += (numberToConvert%(10**(n+1))-numberToConvert%(10**n))/(10**n) * (inputBase**n)
+print(numberToConvert, " in base ", inputBase, " is ", finalVal, "in base 10.")
