12d6846cd87ab2a2350127641c084a1cdb426ddb
rod007vrr <37837157+rod007vrr@users.noreply.github.com>
2018-11-23 12:16:54 -0500

    Update 1-What type of angle.py

--- MYP 1-3 programs/6th Grade/3-Geometry -Trigonometry/1-Lines - Angles/1-What type of angle.py
+++ MYP 1-3 programs/6th Grade/3-Geometry -Trigonometry/1-Lines - Angles/1-What type of angle.py
@@ -1,1 +1,12 @@
-
+print("Hello welcome to the what type of angle calculator. To find what type of agnle is please just enter the angle and recieve your answer.")
+angle = int(input("Angle in degrees:\n"))
+if angle <90:
+    print("A angle of " Angle, " degrees is a acute angle")
+elif angle =90:
+    print("A angle of " Angle, " degrees is a right angle")
+elif angle >90 and angle<180:
+    print("A angle of " Angle, " degrees is a obtuse angle")
+elif angle=180:
+    print("A angle of " Angle, " degrees is a striaght angle")
+elif angle>180:
+    print("A angle of " Angle, " degrees is a reflex angle")
