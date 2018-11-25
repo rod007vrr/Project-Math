b87aef3d2559945a5f21c6fa1f29abaf3c18f25d
rod007vrr <37837157+rod007vrr@users.noreply.github.com>
2018-11-25 09:34:21 -0500

    Create pythagoras.py

---
+++ MYP 1-3 programs/7th grade/3-Geometry and trigonometry/1-Lines and angles/pythagoras.py
@@ -0,0 +1,13 @@
+import math
+print("Hello, welcome to the pythagorean theorem solver. To use find the hypotenuse please input 1 and to find one of the other sides input 2.")
+choice = int(input("What do you want to do?\n"))
+if choice = 1
+    print("You chose to find the hypotenuse, please input the 2 non-hypotenuse lengths")
+    side1 = int(input("Side 1:\n"))
+    side2 = int(input("Side 2:\n"))
+    print("The hypotenuse is", math.sqrt(side1**2 + side2**2), "units long.")
+elif choice = 2
+    print("You chose to find the hypotenuse, please input the 2 non-hypotenuse lengths")
+    notHypotenuse = int(input("Non-Hypotenuse side:\n"))
+    Hypotenuse = int(input("Hypotenuse:\n"))
+    print("The remaining side  is", math.sqrt(Hypotenuse**2 - notHypotenuse**2), "units long.")
