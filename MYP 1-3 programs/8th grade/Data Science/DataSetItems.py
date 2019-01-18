e2fd52a2a7ef9073ba7e477339b1b33008e617e7
rod007vrr <37837157+rod007vrr@users.noreply.github.com>
2019-01-17 22:20:22 -0500

    Create DataSetItems.py

---
+++ MYP 1-3 programs/8th grade/Data Science/DataSetItems.py
@@ -0,0 +1,21 @@
+import statistics
+print("Welcome to the data set finder, please begin inputting your data points and when you are done input end.")
+done = False
+dataset = []
+while done == False:
+    inputtedValue = input("Your data point, input 'end' if done:\n")
+    if inputtedValue == "end":
+        done = True
+    else:
+        dataset.append(int(inputtedValue))
+print("Your data set is", dataset)
+amount = 0
+total = 0
+for n in dataset:
+    total+=n
+    amount+=1
+    average = total/amount
+print("The average is: {}".format(average))
+print("The median is: {}".format(statistics.median(dataset)))
+print("The range is: {}".format(max(dataset)-min(dataset)))
+print("The mode is: {}".format(statistics.mode(dataset)))
